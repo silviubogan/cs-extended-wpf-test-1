5137cf90eb3b7261cec7fd1953399a97994bf423
Silviu Bogan <silviubogan@gmail.com>
2019-08-23 14:34:59 +0300

    More work

--- cs-extended-wpf-test-1/MainWindow.xaml.cs
+++ cs-extended-wpf-test-1/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace cs_extended_wpf_test_1
 {
@@ -25,14 +28,47 @@ namespace cs_extended_wpf_test_1
             InitializeComponent();
         }
 
+        internal Xceed.Wpf.AvalonDock.Layout.Serialization.XmlLayoutSerializer Serializer;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MyFlowViewAnchorable.Float();
-            MyFlowViewAnchorable.IsVisible = false;
-            MyFlowViewAnchorable.AddToLayout(MyDockingManager,
-                Xceed.Wpf.AvalonDock.Layout.AnchorableShowStrategy.Top);
-            MyDataGridAnchorable.AddToLayout(MyDockingManager,
-                Xceed.Wpf.AvalonDock.Layout.AnchorableShowStrategy.Bottom);
+            Serializer = new Xceed.Wpf.AvalonDock.Layout.Serialization.XmlLayoutSerializer(MyDockingManager);
+
+            using (StringWriter writer = new StringWriter())
+            {
+                Serializer.Serialize(writer);
+
+                string xml = writer.ToString();
+
+                XElement doc = XElement.Parse(xml);
+
+                XElement flowViewEl = doc
+                    .Descendants()
+                    .Where(el =>
+                    {
+                        return el.Attribute(XName.Get("ContentId"))?.Value == "MyFlowViewAnchorable";
+                    })
+                    .First();
+
+                flowViewEl.Remove();
+
+                doc.Elements(XName.Get("RootPanel"))
+                    .Descendants(XName.Get("LayoutAnchorablePane"))
+                    .First()
+                    .Add(flowViewEl);
+
+                using (StringReader reader = new StringReader(doc.ToString()))
+                {
+                    Serializer.Deserialize(reader);
+                }
+
+                var la = MyDockingManager.FindName("MyFlowViewAnchorable") as Xceed.Wpf.AvalonDock.Layout.LayoutAnchorable;
+
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    la.IsActive = true;
+                }));
+            }
         }
     }
 }
